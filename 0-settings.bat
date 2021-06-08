c2d2b1c5ae6a43a6837f0a755fbf7b56fef0bebb
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-06-08 14:53:06 +0300

    [clients][portable-windows-cmd] Integrated go client. Refactoring in scripts.

--- 0-settings.bat
+++ 0-settings.bat
@@ -3,8 +3,12 @@ set PSEUDO_HERO_ELEMENTS=BOMBERMAN,BOMB_BOMBERMAN,DEAD_BOMBERMAN
 set BOARD_URL=http://127.0.0.1:8080/codenjoy-contest/board/player/0?code=000000000000
 
 set ROOT=%CD%
+
+set SKIP_TESTS=true
+set CODE_PAGE=65001
+
+set MVNW=mvnw
+set MAVEN_OPTS=-Dmaven.repo.local=%ROOT%\..\.m2\repository
+
 set PSEUDO_CLIENT_HOME=%ROOT%
 set PSEUDO_RULES=%PSEUDO_CLIENT_HOME%\rules
-set SKIP_TESTS=true
-set MAVEN_OPTS=-Dmaven.repo.local=%ROOT%\..\.m2\repository
-set CODE_PAGE=65001
