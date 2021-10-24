d25f1663db041c9c56af61c77692326d63a2b355
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-10-25 00:26:35 +0300

    [clients][pseudo] Separated the build of the application and its launch.

--- 3-run.bat
+++ 3-run.bat
@@ -6,4 +6,7 @@ echo on
 
 call %JAVA% -Dfile.encoding=UTF-8 -jar %ROOT%/app.jar "%GAME_TO_RUN%" "%BOARD_URL%" "%PSEUDO_RULES%"
 
+rem another way to run
+rem call %MVNW% exec:java -Dfile.encoding=UTF-8 -D"exec.mainClass"="com.codenjoy.dojo.PseudoRunner" -D"exec.args"="%GAME_TO_RUN% %BOARD_URL% %PSEUDO_RULES%"
+
 call lib :ask
