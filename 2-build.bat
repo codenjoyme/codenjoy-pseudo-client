d923d28245840917e9405c9218cf87f445ddb883
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-08 02:13:28 +0200

    [clients][java][pseudo][bat] Make build and launch methods identical.

--- 2-build.bat
+++ 2-build.bat
@@ -5,7 +5,11 @@ call lib :color Building pseudo client...
 echo on
 
 call lib :print_color %MVNW% -v
-call %MVNW% clean compile assembly:single -Pjar-with-dependencies -DskipTests=%SKIP_TESTS%
-echo F|xcopy /y %ROOT%\target\client-pseudo-exec.jar %ROOT%\app.jar
+
+call %MVNW% clean install -DskipTests=%SKIP_TESTS%
+
+rem to create executable jar please uncomment lines
+rem call %MVNW% clean compile assembly:single -Pjar-with-dependencies -DskipTests=%SKIP_TESTS%
+rem echo F|xcopy /y %ROOT%\target\client-pseudo-exec.jar %ROOT%\app.jar
 
 call lib :ask
