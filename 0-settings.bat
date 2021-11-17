3cc8f5763c726865771b580d4dddc37bd2bd5963
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-17 11:31:23 +0200

    [clients][pseudo][bat] Added PSEUDO_RULES variable.

--- 0-settings.bat
+++ 0-settings.bat
@@ -4,6 +4,7 @@ echo on
 
 if "%GAME_TO_RUN%"==""          ( set GAME_TO_RUN=mollymage)
 if "%BOARD_URL%"==""            ( set BOARD_URL=http://127.0.0.1:8080/codenjoy-contest/board/player/0?code=000000000000)
+if "%PSEUDO_RULES%"==""         ( set PSEUDO_RULES=.\rules)
 
 set ROOT=%CD%
 
