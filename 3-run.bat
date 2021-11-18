8b4833f136b1a92566a2216bee14697982842abf
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-18 16:13:12 +0200

    [clients][all][bat] Fixed windows path style.

--- 3-run.bat
+++ 3-run.bat
@@ -5,7 +5,7 @@ call lib :color Starting pseudo client...
 echo on
 
 rem run jar
-call %JAVA% -Dfile.encoding=UTF-8 -jar %ROOT%/app.jar "%GAME_TO_RUN%" "%BOARD_URL%" "%PSEUDO_RULES%"
+call %JAVA% -Dfile.encoding=UTF-8 -jar %ROOT%\app.jar "%GAME_TO_RUN%" "%BOARD_URL%" "%PSEUDO_RULES%"
 
 rem build & run (without jar)
 rem call %MVNW% clean compile exec:java -Dfile.encoding=UTF-8 -D"exec.mainClass"="com.codenjoy.dojo.PseudoRunner" -D"exec.args"="%GAME_TO_RUN% %BOARD_URL% %PSEUDO_RULES%"
