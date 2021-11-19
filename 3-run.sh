b9e456078dfd02bb0cde434db8d9f47ee2417aa0
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-19 15:07:10 +0200

    [clients][pseudo][bash] Fixed run.sh - there are three parameters.

--- 3-run.sh
+++ 3-run.sh
@@ -6,9 +6,9 @@ color $COLOR1 "Starting pseudo client..."
 echo
 
 # run jar
-eval_echo "$JAVA -Dfile.encoding=UTF-8 -jar $ROOT/app.jar '$GAME_TO_RUN' '$BOARD_URL'"
+eval_echo "$JAVA -Dfile.encoding=UTF-8 -jar $ROOT/app.jar '$GAME_TO_RUN' '$BOARD_URL' '$PSEUDO_RULES'"
 
 # build & run (without jar)
-# eval_echo "$MVNW clean compile exec:java -Dfile.encoding=UTF-8 -D'exec.mainClass'='com.codenjoy.dojo.PseudoRunner' -D'exec.args'='$GAME_TO_RUN $BOARD_URL'"
+# eval_echo "$MVNW clean compile exec:java -Dfile.encoding=UTF-8 -D'exec.mainClass'='com.codenjoy.dojo.PseudoRunner' -D'exec.args'='$GAME_TO_RUN $BOARD_URL $PSEUDO_RULES'"
 
 ask
