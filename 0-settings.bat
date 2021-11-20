947e9613de6671022874162cc1b5c9d4a0a502ee
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-20 15:33:21 +0200

    [clients][pseudo][bash][docker] Used docker instead of portable.

--- 0-settings.bat
+++ 0-settings.bat
@@ -2,12 +2,13 @@ echo off
 call lib :color Setup variables...
 echo on
 
-if "%GAME_TO_RUN%"=="" ( set GAME_TO_RUN=mollymage)
-if "%BOARD_URL%"==""   ( set BOARD_URL=http://127.0.0.1:8080/codenjoy-contest/board/player/0?code=000000000000)
+if "%GAME_TO_RUN%"==""  ( set GAME_TO_RUN=mollymage)
+if "%BOARD_URL%"==""    ( set BOARD_URL=http://127.0.0.1:8080/codenjoy-contest/board/player/0?code=000000000000)
+if "%PSEUDO_RULES%"=="" ( set PSEUDO_RULES=.\rules\%GAME_TO_RUN%)
 
 set ROOT=%CD%
 
-if "%SKIP_TESTS%"==""  ( set SKIP_TESTS=true)
+if "%SKIP_TESTS%"==""   ( set SKIP_TESTS=true)
 
 set CODE_PAGE=65001
 chcp %CODE_PAGE%
